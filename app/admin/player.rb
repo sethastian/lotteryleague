26cc23d500a1c73e0f62c1bd58e4491ef80d2a28
Seth Hrbek <shrbek@stream9.net>
2019-03-30 11:50:14 -0400

    start refactor

--- app/admin/player.rb
+++ app/admin/player.rb
@@ -6,28 +6,59 @@ ActiveAdmin.register Player do
 active_admin_importable do |model, hash|
 
     #### IMPORT PLAYERS
-    
-    if (hash.to_a[1][0].length >= 5)  
+    # We need to run import twice. once to import players. once to import related players
+    # Make first column 'import_related' to import related
 
-      if (!hash[:nameimport].blank?)
-        player = model.create!(:number => hash[:number], :currentBandName => hash[:band], :name => hash[:nameimport], :instrument => hash[:instrument], :phone => hash[:phone], :email => hash[:email], :band2008 => hash[:band2008], :band2010 => hash[:band2010], :band2013 => hash[:band2013], :description => hash[:description])
-      end
+    first = hash[:first].strip
+    last = hash[:last].strip
+    current_player_name = "#{first} #{last}"
 
-    #### IMPORT RELATED PLAYERS
+    if hash.to_a[0][0].to_s == "import_related"
+      current_player = Player.find_by_email(hash[:email])
 
-    else
-      for i in 2..hash.length-1
-     
-        if (hash.to_a[i][1].blank?)
+      related_players = hash[:list].split(',')
+
+      related_players.each do |related_player|
+        name = related_player.squeeze(' ').strip
+        related_player_obj = Player.find_by_name(name)
+
+        if !related_player_obj
+          Rails.logger.warn("Couldnt find #{name}")
         else
-          player = Player.where(name: hash[:name] ).first
-          related = Player.find_by_name(hash.to_a[i][1])
-          if (!related.nil?)
-            player.related_players << related
+          unless related_player_obj.name == current_player_name
+            current_player.related_players << related_player_obj
           end
         end
       end
-    end
+
+    else
+      if !hash[:first].blank?
+        player = model.create!(:name => current_player_name, :email => hash[:email])
+      end
+    end  
+
+
+    #if (hash.to_a[1][0].length >= 5)  
+
+    #  if (!hash[:nameimport].blank?)
+    #    player = model.create!(:number => hash[:number], :currentBandName => hash[:band], :name => hash[:nameimport], :instrument => hash[:instrument], :phone => hash[:phone], :email => hash[:email], :band2008 => hash[:band2008], :band2010 => hash[:band2010], :band2013 => hash[:band2013], :description => hash[:description])
+    #  end
+
+    #### IMPORT RELATED PLAYERS
+
+    #else
+    #  for i in 2..hash.length-1
+     
+    #    if (hash.to_a[i][1].blank?)
+    #    else
+    #      player = Player.where(name: hash[:name] ).first
+    #      related = Player.find_by_name(hash.to_a[i][1])
+    #      if (!related.nil?)
+    #        player.related_players << related
+    #      end
+    #    end
+    #  end
+    #end
 end
 
 
